4f51a60f7435b46ec616c165e45d6b8d1f3291ac
duanexiao <duanexiao@gmail.com>
2019-06-20 15:32:59 +0800

    fix start command shell bugs

--- Bert-Multi-Label-Text-Classification/start_command.sh
+++ Bert-Multi-Label-Text-Classification/start_command.sh
@@ -3,7 +3,7 @@ wget https://storage.googleapis.com/bert_models/2018_10_18/uncased_L-12_H-768_A-
 cd pybert/model/pretrain
 rm -rf uncased_L-12_H-768_A-12
 unzip uncased_L-12_H-768_A-12.zip
-cd ../..
+cd ../../../
 python convert_tf_checkpoint_to_pytorch.py
 python train_bert_multi_label.py
 python inference.py
