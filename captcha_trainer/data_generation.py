da579c08837d91ec62d99e5a6bde3e4935ec4884
肖景芬 <duanexiao@wuzhi-ai.com>
2019-10-04 23:08:50 +0800

    add captcha_trainer

---
+++ captcha_trainer/data_generation.py
@@ -0,0 +1,21 @@
+import os
+import uuid
+import glob
+from random import randint
+from captcha.image import ImageCaptcha
+from character import *
+
+
+generation_size = 100
+captcha_content = SIMPLE_CHAR_SET["ALPHANUMERIC"]
+captcha_length = 4
+captcha_size = {"height": 50, "width": 150}
+data_save_dir = "./dataset/train_raw"
+if not os.path.exists(data_save_dir):
+    os.makedirs(data_save_dir)
+generator = ImageCaptcha(**captcha_size)
+while len(glob.glob(data_save_dir + "/*.png")) < generation_size:
+    chars = [captcha_content[randint(0, len(captcha_content) - 1)]\
+             for _ in range(captcha_length)]
+    img = generator.generate_image(chars)
+    img.save(data_save_dir + "/" + "".join(chars)+ "_" + str(uuid.uuid4()) +".png")
