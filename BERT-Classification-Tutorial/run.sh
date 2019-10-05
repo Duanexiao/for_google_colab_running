8cf3c855a2d58c699b8531b9aca8754abedd8015
肖景芬 <duanexiao@wuzhi-ai.com>
2019-10-05 09:07:21 +0800

    delete some files

--- BERT-Classification-Tutorial/run.sh
+++
@@ -1,6 +0,0 @@
-python run_classifier.py \
---data_dir=./data \
---vocab_file=./uncased_L-12_H-768_A-12/vocab.txt \
---bert_config_file=./uncased_L-12_H-768_A-12/bert_config.json \
---init_checkpoint=./uncased_L-12_H-768_A-12/bert_model.ckpt \
---output_dir=./model_output
